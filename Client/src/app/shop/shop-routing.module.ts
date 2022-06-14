82dcf0a67b80fb011a208ea27b8ebd5e08a59ec4
Harun Petekkaya <harunpe2107@gmail.com>
2022-06-15 02:40:09 +0300

    routing added

--- Client/src/app/shop/shop-routing.module.ts
+++ Client/src/app/shop/shop-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ShopComponent } from './shop.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
+const routes: Routes = [
+
+  { path: '', component: ShopComponent},
+  { path: ':id', component: ProductDetailComponent }
+
+
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(routes)
+  ],
+  exports:[
+    RouterModule
   ]
 })
 export class ShopRoutingModule { }
